b37a9640d69a0f096544d089ab12b2392d780bde
adinareiter <144619417+adinareiter@users.noreply.github.com>
2023-11-30 14:25:37 -0500

    creates Header.jsx and inputs header, creates Content.jsx and creates App.jsx and replaces code with importing Header, Footer, and Content and displaying it

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="#">Home</a> | <a href="#ProductsIndex">Shop</a> | <a href="#">Account</a> | <a href="#">Cart</a>
+      </nav>
+    </header>
+  );
+}
